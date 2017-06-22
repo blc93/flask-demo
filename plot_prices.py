ebc5e9db0d35cf019d156e929fb7cee8422075e9
blc93 <bobbicoffey@192.168.1.3>
2017-06-22 16:06:58 -0400

    next

---
+++ plot_prices.py
@@ -0,0 +1,13 @@
+from bokeh.charts import TimeSeries
+from bokeh.embed import components
+
+
+def get_plot_ticker_components(ticker, df_data):
+    plot_title = 'Last Month ' + ticker.upper() + ' Closing Price'
+
+    p = TimeSeries(df_data.closing_price, legend=True, title=plot_title, xlabel='Date', ylabel='Prices')
+    p.title.text_font_size = '14pt'
+
+    script, div = components(p)
+
+    return script, div
